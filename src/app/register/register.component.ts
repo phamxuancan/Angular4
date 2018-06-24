42d62f2406b5547065dfaa060896da14ff94c34f
computer\start <dongsonglangle1990@gmail.com>
2018-06-24 22:40:20 +0700

    add view

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,17 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import {Http} from '@angular/http';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'], 
+  providers:[CookieService]
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
+isLogin  = this.cookieService.get('isLogin');
+  constructor(private http:Http,private cookieService:CookieService) { }
 
   ngOnInit() {
   }
   submitRegister(formRegister){
-    console.log(formRegister);
+    this.http.get('http://localhost/chatbot/test').
+    toPromise()
+    .then(res=>res.json()).then(response => 
+      {
+        this.isLogin = 'login';
+        this.cookieService.set( 'isLogin', 'login' );
+      }
+    )
   }
 }
